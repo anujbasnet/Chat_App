b8546f6ef4cecdb30dba10ad50debe5579a94068
Anuj Basnet <anjbas123@gmail.com>
2025-08-31 22:53:00 +0545

    Login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
+import Background from "../assets/images/BackGround.png";
 function Login() {
   return (
-    <div className='login'>
-        Login Page
+    <div className="Login">
+      <div className="glass">
+        <h3
+          style={{
+            color: "white",
+            textAlign: "center",
+            padding: "10px",
+            fontFamily: "Arial, sans-serif",
+          }}
+        >
+          CHIT CHAT
+        </h3>
+        <h3
+          style={{
+            color: "white",
+            fontFamily: "Arial, sans-serif",
+            padding: "10px",
+            paddingLeft: "60px",
+          }}
+        >
+          Login
+        </h3>
+        <p
+          style={{
+            color: "white",
+            fontFamily: "Arial, sans-serif",
+            padding: "10px",
+            paddingLeft: "60px",
+          }}
+        >
+          Email
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
