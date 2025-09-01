229dd418e7ac5c537a0416ed45c153fb503da38c
Anuj Basnet <anjbas123@gmail.com>
2025-09-01 12:11:01 +0545

    Login form UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,36 +5,63 @@ function Login() {
   return (
     <div className="Login">
       <div className="glass">
-        <h3
-          style={{
-            color: "white",
-            textAlign: "center",
-            padding: "10px",
-            fontFamily: "Arial, sans-serif",
-          }}
-        >
-          CHIT CHAT
-        </h3>
-        <h3
-          style={{
-            color: "white",
-            fontFamily: "Arial, sans-serif",
-            padding: "10px",
-            paddingLeft: "60px",
-          }}
-        >
-          Login
-        </h3>
-        <p
-          style={{
-            color: "white",
-            fontFamily: "Arial, sans-serif",
-            padding: "10px",
-            paddingLeft: "60px",
-          }}
-        >
-          Email
-        </p>
+        <div style={{ textAlign: "center" }}>
+          <h3
+            style={{
+              color: "white",
+              fontFamily: "Gilroy-Bold, sans-serif",
+              fontSize: "24px",
+            }}
+          >
+            CHIT CHAT
+          </h3>
+        </div>
+        <div className="FormDiv">
+          <h3
+            style={{
+              color: "white",
+              fontFamily: "Gilroy-Bold, sans-serif",
+              fontSize: "24px",
+            }}
+          >
+            Login
+          </h3>
+          <p
+            className="SmallFonts"
+          >
+            Email
+          </p>
+          <input
+            placeholder="Enter your email"
+            className='inputField'
+      
+          />
+          <p
+            className="SmallFonts"
+          >
+            Password
+          </p>
+          <input
+            placeholder="Enter your password"
+            className='inputField'
+          />
+          <br />
+          <p className="SmallFonts">
+            Forgot Password?
+          </p>
+          <button className="button">Sign in</button>
+          
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <p
+            style={{
+              color: "white",
+              fontFamily: "Gilroy-Medium, sans-serif",
+              fontSize: "13px",
+            }}>
+            or continue with
+          </p>
+          </div>
       </div>
     </div>
   );
