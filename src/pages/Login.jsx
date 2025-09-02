dab8290b49fa9f273a960225f79f1c71d1848bd9
Anuj Basnet <anjbas123@gmail.com>
2025-09-02 20:16:17 +0545

    Usestate_in_React

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import Github from '../assets/images/github-mark.png'
-import Google from '../assets/images/Google__G__logo.svg'
-import Facebook from '../assets/images/facebook.png'
+import Github from "../assets/images/github-mark.png";
+import Google from "../assets/images/Google__G__logo.svg";
+import Facebook from "../assets/images/facebook.png";
 function Login() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const setnewEmail = () => {
+    console.log(Email);
+    setEmail("");
+  };
+  const setnewPassword = () => {
+    console.log(Password);
+    setPassword("");
+  };
   return (
     <div className="Login">
       <div className="glass">
         <div style={{ textAlign: "center" }}>
-          <h3
-           className="heading"
-          >
-            CHIT CHAT
-          </h3>
+          <h3 className="heading">CHIT CHAT</h3>
         </div>
         <div className="FormDiv">
-          <h3
-            className="heading"
-          >
-            Login
-          </h3>
-          <p
-            className="SmallFonts"
-          >
-            Email
-          </p>
+          <h3 className="heading">Login</h3>
+          <p className="SmallFonts">Email</p>
           <input
+            type="email"
+            value={Email}
             placeholder="Enter your email"
-            className='inputField'
-      
+            className="inputField"
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <p
-            className="SmallFonts"
-          >
-            Password
-          </p>
+          <p className="SmallFonts">Password</p>
           <input
+          value={Password}
             placeholder="Enter your password"
-            className='inputField'
+            className="inputField"
+            type="password"
+            onChange={(e)=>setPassword(e.target.value)}
           />
           <br />
-          <p className="SmallFonts">
-            Forgot Password?
-          </p>
-          <button className="button">Sign in</button>
-          
+          <p className="SmallFontsUse">Forgot Password?</p>
+          <button
+            className="button"
+            onClick={() => {
+              setnewEmail();
+              setnewPassword();
+            }}
+          >
+            Sign in
+          </button>
         </div>
         <div style={{ textAlign: "center" }}>
           <p
@@ -52,37 +56,40 @@ function Login() {
               color: "white",
               fontFamily: "Gilroy-Medium, sans-serif",
               fontSize: "13px",
-            }}>
+            }}
+          >
             or continue with
           </p>
-          </div>
-          <div className="signupWith">
-            <p className="signupWith_Button">
-              <img src={Google} className="Icon" />
-            </p>
-            <p className="signupWith_Button">
-              <img src={Github} className="Icon" />
-            </p>
-             <p className="signupWith_Button">
-              <img src={Facebook} className="Icon" />
-            </p>
-          </div>
-          <div style={{display:'flex',paddingLeft:'60px'}}>
-          <p style={{
+        </div>
+        <div className="signupWith">
+          <p className="signupWith_Button">
+            <img src={Google} className="Icon" />
+          </p>
+          <p className="signupWith_Button">
+            <img src={Github} className="Icon" />
+          </p>
+          <p className="signupWith_Button">
+            <img src={Facebook} className="Icon" />
+          </p>
+        </div>
+        <div style={{ display: "flex", paddingLeft: "60px" }}>
+          <p
+            style={{
               color: "white",
               fontFamily: "Gilroy-Medium, sans-serif",
               fontSize: "10px",
-            }}>
+            }}
+          >
             Don't have an account yet?
           </p>
-          
-          <p style={{
-              color: "white",
-              fontFamily: "Gilroy-Medium, sans-serif",
-              fontSize: "10px",
-              fontWeight:'bold',
-            }}> Register for free</p>
-          </div>
+
+          <p
+            className="Register"
+          >
+            {" "}
+            Register for free
+          </p>
+        </div>
       </div>
     </div>
   );
